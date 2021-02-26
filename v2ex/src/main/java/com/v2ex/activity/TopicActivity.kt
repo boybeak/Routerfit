2e4f63e974999b36711e6df0a79449f781e928a6
boybeak <boybeak@gmail.com>
2021-02-27 03:27:51 +0800

    modify v2ex

--- v2ex/src/main/java/com/v2ex/activity/TopicActivity.kt
+++ v2ex/src/main/java/com/v2ex/activity/TopicActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatTextView
+import com.github.boybeak.irouter.IRouter
+import com.github.boybeak.irouter.core.annotation.Inject
 import com.github.boybeak.irouter.core.annotation.RoutePath
 import com.v2ex.R
 import com.v2ex.api.model.Topic
@@ -11,15 +13,16 @@ import com.v2ex.api.model.Topic
 @RoutePath("topic/detail")
 class TopicActivity : AppCompatActivity() {
 
+    @Inject("topic")
     private var topic: Topic? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_topic)
 
-//        val topic = intent.getParcelableExtra<Topic>("topic")!!
+        IRouter.inject(this, intent)
 
-//        findViewById<AppCompatTextView>(R.id.titleTV).text = topic.content
+        findViewById<AppCompatTextView>(R.id.titleTV).text = topic!!.content
 
     }
 
